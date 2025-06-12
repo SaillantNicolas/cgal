e96a6a50b272e632d68fd6a7f80cd580b70aaac3
Nicolas Saillant <saillantnicolas01@gmail.com>
2025-06-12 08:48:59 +0200

    fix: move echo statements for better visibility in install_windows.sh

--- .github/install_windows.sh
+++ .github/install_windows.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 set -ex
 
+echo "============================"
+echo "Installing vcpkg dependencies for Windows"
 cd $GITHUB_WORKSPACE
 git clone https://github.com/microsoft/vcpkg.git
 ls vcpkg
