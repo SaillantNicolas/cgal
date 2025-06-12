eb22fc72cd95c2ebcce36556b7f23f792e169a50
Nicolas Saillant <saillantnicolas01@gmail.com>
2025-06-12 12:16:30 +0200

    fix: update conan profile to set C++ standard version in install_windows.sh

--- .github/install_windows.sh
+++ .github/install_windows.sh
@@ -6,6 +6,8 @@ cd "$GITHUB_WORKSPACE"
 pip install conan
 
 conan profile detect --force
+conan profile update settings.compiler.cppstd=17 default
+
 cp .github/conanfile.txt Lab/demo/
 
 cd Lab/demo
