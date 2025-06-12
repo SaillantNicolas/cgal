0506aab686728492dfe1e2751cab24203a759711
Nicolas Saillant <saillantnicolas01@gmail.com>
2025-06-12 12:36:40 +0200

    fix: ensure C++ standard version is set correctly in conan profile within install_windows.sh

--- .github/install_windows.sh
+++ .github/install_windows.sh
@@ -8,7 +8,12 @@ pip install conan
 conan profile detect --force
 
 PROFILE_PATH=$(conan profile path default)
-echo "compiler.cppstd=17" >> "$PROFILE_PATH"
+
+if grep -q '^compiler.cppstd=' "$PROFILE_PATH"; then
+    sed -i 's/^compiler\.cppstd=.*/compiler.cppstd=17/' "$PROFILE_PATH"
+else
+    echo "compiler.cppstd=17" >> "$PROFILE_PATH"
+fi
 
 cp .github/conanfile.txt Lab/demo/
 
