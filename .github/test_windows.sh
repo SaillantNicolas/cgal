fa10e44436806cc98ff4bee98a183ec5a558659b
Nicolas Saillant <saillantnicolas01@gmail.com>
2025-06-19 12:53:52 +0200

    fix: enhance logging in test_windows.sh to display CGAL_DIR, GITHUB_WORKSPACE, and plugin details

--- .github/test_windows.sh
+++ .github/test_windows.sh
@@ -3,12 +3,18 @@ set -ex
 
 FACTOR=$1
 export CGAL_DIR=$(pwd)
+echo "CGAL_DIR=${CGAL_DIR}"
+echo "GITHUB_WORKSPACE=${GITHUB_WORKSPACE}"
 cd Lab/demo
 cmake -S Lab -B build -DCMAKE_PREFIX_PATH="${CGAL_DIR}/Lab/demo/build"
-
+echo "Contenu de build/"
+find build
 LIST_OF_PLUGINS=$(cmake --build build --config Release --target help |& grep 'plugin$' | cut -d\  -f2)
+echo "Liste des plugins détectés :"
+echo "${LIST_OF_PLUGINS}"
 PLUGINS_ARRAY=(${LIST_OF_PLUGINS})
 NB_OF_PLUGINS=${#PLUGINS_ARRAY[@]}
+echo "Nombre de plugins : ${NB_OF_PLUGINS}"
 DEL=$(($NB_OF_PLUGINS / 4))
 
 cd build
