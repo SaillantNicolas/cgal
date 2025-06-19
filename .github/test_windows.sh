0ab76a7dbf1cca27eff4f959fd41d3415df345af
Nicolas Saillant <saillantnicolas01@gmail.com>
2025-06-19 11:51:14 +0200

    fix: update test_windows.sh to use --config Release in cmake build commands

--- .github/test_windows.sh
+++ .github/test_windows.sh
@@ -6,10 +6,10 @@ export CGAL_DIR=$(pwd)
 cd Lab/demo
 cmake -S Lab -B build -DCMAKE_PREFIX_PATH="${CGAL_DIR}/Lab/demo/build"
 
-LIST_OF_PLUGINS=$(cmake --build build -t help |& grep 'plugin$' | cut -d\  -f2)
+LIST_OF_PLUGINS=$(cmake --build build --config Release --target help |& grep 'plugin$' | cut -d\  -f2)
 PLUGINS_ARRAY=(${LIST_OF_PLUGINS})
 NB_OF_PLUGINS=${#PLUGINS_ARRAY[@]}
 DEL=$(($NB_OF_PLUGINS / 4))
 
 cd build
-cmake --build . --target ${PLUGINS_ARRAY[@]:$(($FACTOR * $DEL)):$((($FACTOR + 1) * $DEL))}
+cmake --build . --config Release --target ${PLUGINS_ARRAY[@]:$(($FACTOR * $DEL)):$((($FACTOR + 1) * $DEL))}
